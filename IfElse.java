aafa3db7fe0817019d4a2c2fda0a0c62349b671f
Beatris <ravagnanibeatris@gmail.com>
2022-03-10 16:39:31 -0300

    Exercicio ajustado

--- IfElse.java
+++ IfElse.java
@@ -1,22 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
 public class IfElse {
     public static void main(String[] args) {
         java.util.Scanner leitor = new Scanner (System.in);
         System.out.printf ("Digite sua nota\n");
         int nota = leitor.nextInt();
-        if (nota >= 6)
+        if (nota >= 6){
         System.out.printf("Parabens");
         System.out.printf(
-            "A nota %d garante aprovação\n"),
+            "A nota %d garante aprovação\n",
             nota
             );
-            else (
-                System.out.println(
-                    "Com %d você reprova, infelizmente\n",
+    }
+
+            else 
+                System.out.printf(
+                    "Com %d voce reprova",
                     nota
-                );
-                leitor.close();
-            ) 
+                    );
+            
+            
+            leitor.close();
+        
 
     }
 }
